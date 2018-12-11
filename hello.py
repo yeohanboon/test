413951baec0eb0d4ebb69362f29692febce1378e
yeohanboon <45782988+yeohanboon@users.noreply.github.com>
2018-12-11 16:33:58 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,7 +7,10 @@ Created on Tue Dec 11 11:07:33 2018
 
 from flask import Flask
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+ 
+@app.route("/")
+def hello():
+return "Hello World!"
+ 
+if __name__ == "__main__":
+app.run()
